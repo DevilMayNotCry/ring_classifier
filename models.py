f34d0cd33fc38d240c31f4abc13a4e929745f8d8
DevilMayNotCry <46841326+DevilMayNotCry@users.noreply.github.com>
2021-11-29 18:00:40 +0200

    Update models.py

--- models.py
+++ models.py
@@ -4,6 +4,32 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from utils import img_width, img_height, data_augmentation
 
+
+ring_style = Sequential([
+    data_augmentation,
+    layers.Rescaling(1./255),
+    layers.Conv2D(16, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Flatten(),
+    layers.Dropout(0.5),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(6, activation='softmax')
+])
+
 model_style = Sequential([
     data_augmentation,
     layers.Rescaling(1./255),
