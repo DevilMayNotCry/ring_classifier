a1334bad59b6dc9b001f59fe163d5695b714ab86
DevilMayNotCry <46841326+DevilMayNotCry@users.noreply.github.com>
2021-12-09 10:00:57 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,13 +4,22 @@ from tensorflow.keras import layers
 img_height = 256
 img_width = 256
 
-metal_class_names =  []
-
 data_augmentation = keras.Sequential(
   [
     layers.RandomFlip("horizontal",
                       input_shape=(img_height,
                                   img_width,
+                                  3)),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+  ]
+)
+
+data_augmentation = keras.Sequential(
+  [
+    layers.RandomFlip("horizontal",
+                      input_shape=(512,
+                                  512,
                                   3)),
     layers.RandomRotation(0.1),
     layers.RandomZoom(0.1),
