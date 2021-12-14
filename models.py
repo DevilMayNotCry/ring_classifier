8fbfd616895e61a7f76fb58447f59002e838beaa
DevilMayNotCry <46841326+DevilMayNotCry@users.noreply.github.com>
2021-12-14 09:47:56 +0200

    Update models.py

--- models.py
+++ models.py
@@ -33,28 +33,93 @@ model_material_new = Sequential([
     layers.Dense(9, activation='softmax')
 ])
 
+model_style_bigger = Sequential([
+    shape_augmentation,
+    layers.Rescaling(1./255),
+
+    layers.Conv2D(16, 5, padding='same', activation='relu'),
+    layers.BatchNormalization((),
+    layers.Conv2D(16, 5, strides=2, padding='same', activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.2)
+     
+    layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.BatchNormalization((),
+    layers.Conv2D(32, 5, strides=2, padding='same', activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.2)
+
+    layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.BatchNormalization((),
+    layers.Conv2D(32, 5, strides=2, padding='same', activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.2)
+
+    layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.BatchNormalization((),
+    layers.Conv2D(64, 5, strides=2, padding='same', activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.4)
+
+    layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.BatchNormalization((),
+    layers.Conv2D(64, 5, strides=2, padding='same', activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.4)
+
+    layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.BatchNormalization((),
+    layers.Conv2D(64, 5, strides=2, padding='same', activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.4)
+
+    layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.BatchNormalization((),
+    layers.Conv2D(64, 5, strides=2, padding='same', activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.4)
+
+    layers.Flatten(),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(9, activation='softmax')
+])
+
 model_material_2 = Sequential([
     data_augmentation,
     layers.Rescaling(1./255),
     layers.Conv2D(16, 5, padding='same', activation='relu'),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
+    
     layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
+    
     layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
+    
     layers.Conv2D(32, 5, padding='same', activation='relu'),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
+    
     layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
+    
     layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
+    
     layers.Conv2D(64, 5, padding='same', activation='relu'),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
 
     layers.Flatten(),
     layers.Dropout(0.5),
     layers.Dense(256, activation='relu'),
-    layers.Dense(256, activation='relu'),
+    layers.Dense(128, activation='relu'),
     layers.Dense(9, activation='softmax')
 ])
 
